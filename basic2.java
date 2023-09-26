90d0195f4ccb2ae633e09192c5c536ec94bc6a4f
KIRAN-KUMAR-K3 <18kirankumar.k03@gmail.com>
2023-09-26 21:48:54 +0530

    basic java
    this are the some basic java programs.

---
+++ basic2.java
@@ -0,0 +1,11 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int age = 25;
+        double salary = 50000.50;
+        String name = "John";
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: $" + salary);
+    }
+}
---
+++ basic9.java
@@ -0,0 +1,8 @@
+public class StringDemo {
+    public static void main(String[] args) {
+        String message = "Hello, World!";
+        System.out.println("Length: " + message.length());
+        System.out.println("Uppercase: " + message.toUpperCase());
+        System.out.println("Substring: " + message.substring(0, 5));
+    }
+}
